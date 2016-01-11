2c03f955381318cdf3a37e165425639ffc55b69b
bmtreuherz <bmtreuherz@ufl.edu>
2016-01-11 15:01:33 -0500

    Added list adding/ deleting

--- listingController.js
+++ listingController.js
@@ -1,14 +1,25 @@
-angular.module('listings').controller('ListingsController', ['$scope', 'Listings', 
+angular.module('listings').controller('ListingsController', ['$scope', 'Listings',
   function($scope, Listings) {
     $scope.listings = Listings;
     $scope.detailedInfo = undefined;
+    $scope.newListing = {};
+    $scope.currentDetail = {};
 
-    /* 
-      Implement these functions in the controller to make your application function 
-      as described in the assignment spec. 
+    /*
+      Implement these functions in the controller to make your application function
+      as described in the assignment spec.
      */
-    $scope.addListing = function() {};
-    $scope.deleteListing = function(index) {};
-    $scope.showDetails = function(index) {};
+    $scope.addListing = function() {
+      $scope.listings.push($scope.newListing);
+      $scope.newListing = {};
+    };
+    $scope.deleteListing = function(index) {
+      console.log("Delete");
+      $scope.listings.splice(index, 1);
+    };
+    $scope.showDetails = function(index) {
+      console.log("Details");
+      $scope.currentDetail = $scope.listings[index];
+    };
   }
 ]);

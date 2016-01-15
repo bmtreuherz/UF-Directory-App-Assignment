04cce6af08605a9be02f095454a5b19d2749c96c
bmtreuherz <bmtreuherz@ufl.edu>
2016-01-14 22:13:31 -0500

    Finished adding queryies

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,42 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
 
 /* Connect to your database */
+mongoose.connect(config.db.uri);
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+// Read in the json file
+var listings;
+fs.readFile('listings.json', 'utf8', function (err, data) {
+  if (err)  throw err;
+
+  listings = JSON.parse(data);
+
+  // Loop through the listings and save each to the db
+  listings.entries.forEach(function(lst){
+    var newListing = Listing(lst);
+
+    newListing.save(function(err){
+      if (err) throw err;
+
+      console.log("Listing Created.");
+    })
+  })
+});
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
 
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- queries.js
+++ queries.js
@@ -1,30 +1,78 @@
 /* Fill out these functions using Mongoose queries*/
+var mongoose = require('mongoose'),
+    Listing = require('./ListingSchema.js'),
+    config = require('./config');
+
+/* Connect to the database */
+mongoose.connect(config.db.uri);
 
 var findLibraryWest = function() {
-  /* 
+  /*
     Find the document that contains data corresponding to Library West,
-    then log it to the console. 
+    then log it to the console.
    */
+   Listing.find({name: 'Library West'}, function(err, listing){
+     if (err) throw err;
+
+     // output the found listing
+     console.log('Library West:\n')
+     console.log(listing + '\n');
+   });
 };
 var removeCable = function() {
   /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
+    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed
     on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
+    and remove this listing from your database and log the document to the console.
    */
-};
-var updatePhelpsMemorial = function() {
-  /*
-    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-   */
-};
-var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+   Listing.findOne({code: 'CABL'}, function(err, listing){
+     if (err) throw err;
+
+     if(listing){
+       // Log the document to the console.l
+       console.log('Cable Document:\n')
+       console.log(listing + '\n');
+
+       // Remove the listing.
+       listing.remove(function(err){
+         if(err) throw err;
+
+         console.log("Listing successfully deleted." + '\n');
+       });
+     }else{
+       console.log('The CABL document does not exist\n');
+     }
+
+   })
 };
 
+var updatePhelpsMemorial = function() {
+  /*
+    Phelps Memorial Hospital Center's address is incorrect. Find the listing, update it, and then
+    log the updated document to the console.
+   */
+  Listing.findOneAndUpdate({code: 'PHL'}, {address: '100 Phelps Lab P.O. Box 116350 Gainesville, FL  32611'}, { 'new': true }, function(err, listing){
+    if(err) throw err;
+
+    console.log('Update Phelps address\n');
+    console.log(listing + '\n');
+  });
+};
+var retrieveAllListings = function() {
+  /*
+    Retrieve all listings in the database, and log them to the console.
+   */
+   Listing.find({}, function(err, listing){
+     if (err) throw err;
+
+     // Display the listings in the console.
+     console.log("All listings\n");
+     console.log(listing + '\n');
+   })
+
+};
+
+// Note the console logging may appear out of order as callbacks come in.
 findLibraryWest();
 removeCable();
 updatePhelpsMemorial();
